cbd0af623cd57044e083bc8e0f0c3757df21cf01
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2025-03-24 18:57:13 +0100

    step5 is okay

--- gestion-depenses/bootstrap/app.php
+++ gestion-depenses/bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         // Ajout de notre middleware personnalisé
         $middleware->alias([
             'active.user' => \App\Http\Middleware\CheckUserIsActive::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
